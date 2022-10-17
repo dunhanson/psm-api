e145b384614947c180e4d1102ef282f9cbf6203b
dunhanson <dunhanson@gmail.com>
2022-10-17 21:38:26 +0800

    feat: first commit

---
+++ src/main/java/site/dunhanson/pms/api/PmsApiApplication.java
@@ -0,0 +1,13 @@
+package site.dunhanson.pms.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmsApiApplication.class, args);
+    }
+
+}
